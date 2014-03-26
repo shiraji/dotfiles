3698bc0227e8412bf57e6a44e8ca28853fd34d74
shiraji <isogai.shiraji@gmail.com>
2014-03-26 13:34:02 +0900

    curlのパスの修正を忘れていた

--- vim_install.sh
+++ vim_install.sh
@@ -9,7 +9,7 @@ wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2 -P ${SRC_DIR}
 tar jxvf ${SRC_DIR}/vim-7.4.tar.bz2 -C ${SRC_DIR}
 mkdir -p ${WORK_DIR}/patches
 #http://ftp.vim.org/pub/vim/patches/7.4/
-curl -O 'ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.[001-218]' -o ${WORK_DIR}/patches
+curl -o ${WORK_DIR}/patches/7.4.#1 'ftp://ftp.vim.org/pub/vim/patches/7.4/7.4.[001-218]'
 
 # makeとかはcurrentフォルダが重要なので、移動する。
 cd ${WORK_DIR}
