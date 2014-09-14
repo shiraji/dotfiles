267a842fb82d1d71a4009fe6806856e87c140cf3
shiraji <isogai.shiraji@gmail.com>
2014-09-15 08:16:21 +0900

    スペースを無視するように。setup.shのスペースを統一

--- setup.sh
+++ setup.sh
@@ -24,56 +24,56 @@ fi
 
 for file in ${DOT_FILES[@]}
 do
-	currentFile=$HOME/$file
-	if [ -a $currentFile ]; then
-		if [ ! -L $currentFile ]; then
-			#シンボリックリンクでなかったら、ファイルをバックアップ。
-			backupFile=$currentFile.`date "+%Y%m%d%H%M%S"`
-			cp -prf $currentFile $backupFile
-			echo "Create backup: $backupFile"
-		fi
-		#ファイルの削除。
-		unlink $currentFile
-		echo "Delete $currentFile"
-	fi
-	#リンクを張る
+  currentFile=$HOME/$file
+  if [ -a $currentFile ]; then
+    if [ ! -L $currentFile ]; then
+      #シンボリックリンクでなかったら、ファイルをバックアップ。
+      backupFile=$currentFile.`date "+%Y%m%d%H%M%S"`
+      cp -prf $currentFile $backupFile
+      echo "Create backup: $backupFile"
+    fi
+    #ファイルの削除。
+    unlink $currentFile
+    echo "Delete $currentFile"
+  fi
+  #リンクを張る
 
-	#絶対パスを探す
-	_dir=$(dirname $HOME/dotfiles/setup.sh)
-	_currentFileDir=$(dirname ${currentFile})
+  #絶対パスを探す
+  _dir=$(dirname $HOME/dotfiles/setup.sh)
+  _currentFileDir=$(dirname ${currentFile})
 
-	#シンボリックリンク作成対象のフォルダが存在していない場合、フォルダを作成する。
-	if [ ! -d "${_currentFileDir}" ]; then
-		mkdir -p ${_currentFileDir}
-		echo "Create directory: ${_currentFileDir}"
-	fi
+  #シンボリックリンク作成対象のフォルダが存在していない場合、フォルダを作成する。
+  if [ ! -d "${_currentFileDir}" ]; then
+    mkdir -p ${_currentFileDir}
+    echo "Create directory: ${_currentFileDir}"
+  fi
 
-	ln -s ${_dir}/$file $currentFile
-	echo "Create symbolic link: $currentFile"
+  ln -s ${_dir}/$file $currentFile
+  echo "Create symbolic link: $currentFile"
 
-	if [ "$file" = $VIMRC ]; then
+  if [ "$file" = $VIMRC ]; then
     # vim74をインストールする
     bash ./vim_install.sh
 
-		#neobundleをインストールする
-		mkdir -p $HOME/.vim/bundle/
-		git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+    #neobundleをインストールする
+    mkdir -p $HOME/.vim/bundle/
+    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
     bash ./neobundle_install.sh
 
-		#NERDTreeのプラグインをインストールする。
-		git clone git://gist.github.com/205807 ~/.vim/bundle/nerdtree/nerdtree_plugin/205807
-	fi
+    #NERDTreeのプラグインをインストールする。
+    git clone git://gist.github.com/205807 ~/.vim/bundle/nerdtree/nerdtree_plugin/205807
+  fi
 
-	if [ "$file" = $ZSHRC ]; then
-		#oh-my-zshの設定を持ってくる。
-		git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+  if [ "$file" = $ZSHRC ]; then
+    #oh-my-zshの設定を持ってくる。
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
     # .zshフォルダの作成
     mkdir -p ~/.zsh/
 
     # zshのincremetal searchできるようにする
     curl -o ~/.zsh/incr-0.2.zsh -L http://mimosa-pudica.net/src/incr-0.2.zsh
-	fi
+  fi
 
   if [ "$file" = $SCREENRC ]; then
     # screenのソケットの保存先をHOME直下にする
