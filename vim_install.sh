2ebf9277a3866b8caf24d65a52c72ca22fa5b1cf
shiraji <isogai.shiraji@gmail.com>
2014-12-08 23:52:32 +0900

    wgetの設定が曖昧だったので、修正

--- vim_install.sh
+++ vim_install.sh
@@ -26,7 +26,7 @@ __get_patch() {
 
 # vim本体の準備
 mkdir -p ${SRC_DIR}
-wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2 -p ${SRC_DIR}
+wget -O ${SRC_DIR}/vim-7.4.tar.bz2 ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
 tar jxvf ${SRC_DIR}/vim-7.4.tar.bz2 -C ${SRC_DIR}
 
 # パッチの準備
